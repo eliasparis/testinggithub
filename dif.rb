7473042b6493a304ef88f799ff12cd8370a308e5
Eli <55drk1@gmail.com>
2015-07-09 10:45:19 +0200

    second commit

--- dif.rb
+++ dif.rb
@@ -23,5 +23,30 @@ class Car
 	end
 
 
+class Bike
+	attr_accessor :sound, :nof_wheels
+	def initialize
+		@sound = "wiiiiii"
+		@n_of_wheels = 2
+	end
+
+	def wheels
+		@n_of_wheels	
+	end
+
+	def the_noise
+		@sound
+	end
+
+end
+
+class Car
+	attr_accessor :sound, :n_of_wheels
+	def initialize
+		@sound = "bruuum"
+		@n_of_wheels = 4
+	end
+
+
 
 
